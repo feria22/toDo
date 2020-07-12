fa977926937e74744bd460f5e5e24a2e8e35a9bd
Viktoriia <mw3feria@gmail.com>
2020-07-12 22:57:11 +0200

    Add status for completed and uncompleted tasks

---
+++ src/app/views/statistics/statistics.component.ts
@@ -0,0 +1,27 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.scss']
+})
+export class StatisticsComponent implements OnInit{
+
+  @Input() statusOfTasks
+  @Output() selectStatus= new EventEmitter<boolean>()
+  @Input('status')
+  set setStatus(status){
+    this.status=status
+  }
+  status:boolean
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  onClickStatus(status: boolean) {
+    this.selectStatus.emit(status)
+  }
+}
